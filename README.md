d83c05e1511349867c8499c42455dc449bbbc1f6
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-09-21 19:56:54 +0100

    Update documentation and fixed animation bug in frames cache

--- README.md
+++ README.md
@@ -1,2 +1,135 @@
-# Total Internal Reflection Simulator
- Simple simulation of the phenomena of total internal reflection (TIR) usefull as a teaching resource
+
+# Total Internal Reflection (TIR) Visualizer
+### Author: Adill Al-Ashgar
+
+
+## Introduction
+Simple simulation of the phenomena of total internal reflection (TIR) usefull as a teaching resource. Inspired by QuantumBoffin and his simple and beautiful [Total Internal Reflection video](https://www.youtube.com/watch?v=NAaHPRsveJk).
+
+<img src="Images/Mixed.png" width="600">
+
+
+This Python script visualizes the phenomenon of Total Internal Reflection (TIR) using Matplotlib. Total Internal Reflection occurs when light traveling in a medium with a higher refractive index encounters a boundary with a medium of lower refractive index at an angle of incidence greater than the critical angle. In this simulation, you can control the angle of incidence and the refractive index of the material.
+
+
+
+[![Language](https://img.shields.io/badge/language-Python-blue.svg)](https://www.python.org/)
+
+
+
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Program Controls](#program-controls)
+- [How it Works](#how-it-works)
+
+- [Roadmap](#roadmap)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Equations](#equations)
+- [License](#license)
+- [Contributions](#contributions)
+- [Contact](#contact)
+
+
+## Program Controls 
+
+Once the simulation window appears, you can interact with the following components:
+
+   - **TURN LASER ON/OFF Button**: Toggle the laser beam on and off.
+
+   - **Material Refractive Index Slider**: Adjust the refractive index of the material to observe how it affects the behavior of light at the boundary.
+
+   - **Incoming Angle (Deg) Slider**: Control the angle of incidence of the laser beam.
+
+Observe the behavior of the laser beam as it interacts with the material boundary. You can see the reflection and refraction of the beam based on the angle of incidence and the refractive index.
+
+The simulation also displays current values for the angle of incidence, angle of refraction (if applicable), and angle of reflection (if applicable).
+
+## How it Works
+
+This script simulates the TIR phenomenon by calculating the angles of reflection and refraction using Snell's Law. It visualizes the laser beam, its reflection, and refraction, and updates the display in real-time based on user input.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## 1. Introduction
+
+Reflection and refraction are two fundamental processes that occur when light encounters the boundary between different media. Total internal reflection is a unique phenomenon associated with refraction under specific conditions. In this document, we delve into the principles governing these optical phenomena, exploring their significance in the field of optics, physics, and various technological applications.
+
+## 2. Reflection
+Reflection is the process by which light waves bounce off the surface of an object or material, following the law of reflection. According to this law, the angle of incidence (θᵢ) is equal to the angle of reflection (θᵣ), and both angles are measured with respect to the surface normal (Fig. 1).
+
+Mathematically, this can be expressed as:
+
+$$θᵢ = θᵣ$$
+
+<img src="Images/Ref.png" width="600">
+
+
+
+
+## 3. Refraction
+Refraction is the bending of light as it passes from one medium into another with a different refractive index (Fig. 2). This bending occurs due to the change in the speed of light in different media, as described by Snell's Law:
+
+$$n₁ \cdot \sin(θ₁) = n₂ \cdot \sin(θ₂)$$
+
+Where:
+- $n₁$ and $n₂$ are the refractive indices of the first and second media, respectively.
+- $θ₁$ is the angle of incidence, and $θ₂$ is the angle of refraction.
+
+<img src="Images/Transmission.png" width="600">
+
+
+
+## 4. Total Internal Reflection
+Total internal reflection occurs when light travels from a medium with a higher refractive index to one with a lower refractive index at an angle of incidence greater than the critical angle (Fig. 3). Beyond this critical angle, no refraction occurs, and all the light is reflected back into the high-index medium.
+
+The critical angle (\(θ_c\)) can be calculated using:
+
+$$θ_c = \arcsin\left(\frac{n₂}{n₁}\right)$$
+
+<img src="Images/TIR.png" width="600">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## License
+This project is not currently licensed. 
+
+## Contributions
+Contributions to this codebase are welcome! If you encounter any issues, bugs or have suggestions for improvements please open an issue or a pull request on the [GitHub repository](https://github.com/Adillwma/DeepClean-Noise-Suppression-for-LHC-B-Torch-Detector).
+
+## Contact
+For any further inquiries or for assistance, please feel free to reach out to me at adill@neuralworkx.com.
+
+
+
+
+
+
+
+
--- Total_internal-reflection_tool.py
+++ Total_internal-reflection_tool.py
@@ -1,40 +1,36 @@
-
 #%% - Dependencies
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from matplotlib.animation import PillowWriter
-from matplotlib.widgets import Slider, Button, TextBox
+from matplotlib.widgets import Slider, Button
 
+"""
+532nm green laser light is used in this example. 
+Because laser light is Monochromatic - It contains only one specific wavelength and hence one color.
+"""
 
-import math
 
 def calculate_reflection_refraction(θ1, n1, n2):
     if θ1 == 0:
         return 180, 0, 1
 
-    else:
-        # Calculate the angle of refraction (θ2) using Snell's Law
-        sin_θ2 = (n1 / n2) * math.sin(math.radians(θ1))
-        if -1 <= sin_θ2 <= 1:
-            θ2 = math.degrees(math.asin(sin_θ2)) + 180
-            if θ1 + θ2 != 0:
-                reflection = (math.sin(math.radians(θ1 - θ2)) / math.sin(math.radians(θ1 + θ2)))**2
-            else:
-                reflection = 1
+    # Calculate the angle of refraction (θ2) using Snell's Law
+    sin_θ2 = (n1 / n2) * np.sin(np.radians(θ1))
+    if -1 <= sin_θ2 <= 1:
+        θ2 = np.degrees(np.arcsin(sin_θ2)) + 180
+        if θ1 + θ2 != 0:
+            reflection = (np.sin(np.radians(θ1 - θ2)) / np.sin(np.radians(θ1 + θ2)))**2
         else:
-            # Handle the case when sin_θ2 is outside the valid range, e.g., set θ2 to a default value or raise an exception.
-            # You may need to adjust this based on your specific requirements.
-            θ2 = 0  # Just a placeholder value, the alpha value will be set to 0 so the line will not be visible 
-            reflection = 1  # 100% reflection
+            reflection = 1
+    else:
+        # Handle the case when sin_θ2 is outside the valid range, e.g., set θ2 to a default value or raise an exception.
+        # You may need to adjust this based on your specific requirements.
+        θ2 = 0  # Just a placeholder value, the alpha value will be set to 0 so the line will not be visible 
+        reflection = 1  # 100% reflection
 
+    refraction = 1 - reflection  # Amount of light refracted
 
-
-        refraction = 1 - reflection  # Amount of light refracted
-
-        return θ2, reflection, refraction
-
-
+    return θ2, reflection, refraction
 
 def init():
     global incident_beam, reflected_beam, refracted_beam, incident_glow, reflected_glow, refracted_glow # Declare incident_beam and laser_state_on as global variables
@@ -54,13 +50,15 @@ def animate(i):
         # add the data to the incident_beam object 
         incident_beam.set_data([0, np.deg2rad(incoming_angle)], [0, 1])
         incident_glow.set_data([0, np.deg2rad(incoming_angle)], [0, 1])
-                
+       
         refraction_angle, reflection_component, refraction_component = calculate_reflection_refraction(incoming_angle, refractive_index, n1)  # Careful to note that beam is coming from the material to the air so the ordering of the refractive indices is reversed
+        
         reflected_beam.set_data([0, -np.deg2rad(incoming_angle)], [0, 1])
         reflected_glow.set_data([0, -np.deg2rad(incoming_angle)], [0, 1])
         reflected_glow.set_alpha(0.3 * reflection_component)
         reflected_beam.set_alpha(reflection_component)
         reflected_glow.set_linewidth(6 - (refraction_component * 2))
+
         refracted_beam.set_data([0, np.deg2rad(refraction_angle)], [0, 1])
         refracted_glow.set_data([0, np.deg2rad(refraction_angle)], [0, 1])
         refracted_beam.set_alpha(refraction_component)
@@ -82,8 +80,6 @@ def animate(i):
 
         data_box_2.set_text(f'Current Values\n\nAngle of Refraction: {refraction_angle}°\nAngle of Reflection: {reflection_angle}°')
 
-
-
     else:
         incident_beam.set_data([], [])
         reflected_beam.set_data([], [])
@@ -102,87 +98,83 @@ def laser_on_off_switch(event):
         button_on_off.label.set_text('TURN LASER\nON')
         button_on_off.color = 'lightblue'
         laser_state_on = False
-        print('Laser Turned OFF')
+
     else:
         button_on_off.label.set_text('TURN LASER\nOFF')
         laser_state_on = True
         button_on_off.color = 'red'
-        print('Laser Turned ON')
 
 def update_refractive_index(val):
     global refractive_index
     refractive_index = refractive_index_slider.val
-    print('Refractive Index: ', refractive_index)
 
 def update_incoming_angle(val):
     global incoming_angle
     incoming_angle = incoming_angle_slider.val
-    print('Incoming Angle: ', incoming_angle)
 
-            
-initial_refractive_index = 1.5 # n
-initial_incoming_angle = 0     # Degrees   #### ANGLE OF INCIDENCE!!!!
-material_radius = 0.4  # Radius of the material
-fps = 30  # Frames per second for the animation 
 
-# Initial states
+# Internal Program Settings
+n1 = 1.0                          # Refractive index of the external medium (e.g., air)            
+initial_refractive_index = 1.5    # n2 # Refractive index of the material
+initial_incoming_angle = 0        # Degrees   #### ANGLE OF INCIDENCE!!!!
+material_radius = 0.4             # Radius of the material
+fps = 30                          # Frames per second for the animation 
+
+# Initialise states
 refractive_index = initial_refractive_index
 incoming_angle = initial_incoming_angle
 laser_state_on = False
 
-n1 = 1.0  # Refractive index of the external medium (e.g., air)
 
-
-### Compute ###
+#%% - Genrating Animation
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'}, figsize=(10, 10))
-# Rotate the 0 angle (e.g., 45 degrees counterclockwise)
-ax.set_theta_offset(np.pi*1.5)  # Set the theta_offset property
 
-
-
-
-
-
-
+# Rotate the 0 angle
+ax.set_theta_offset(np.pi*1.5) 
 
 # Set custom theta tick labels from 0 to -180 and 180 to 0
-theta_labels = [0, 45, 90, 135, 180, -135, -90, -45]  # Custom labels
-ax.set_xticklabels([str(label) + '°' for label in theta_labels])  # Add degree symbol
+theta_labels = [0, 45, 90, 135, 180, -135, -90, -45]
+ax.set_xticklabels([str(label) + '°' for label in theta_labels])  
 
-plt.subplots_adjust(bottom=0.3)  # Adjust the bottom margin to make space for sliders
+# Adjust the bottom margin to make space for sliders
+plt.subplots_adjust(bottom=0.3)  
 
+# Add the title above the sliders
+plt.figtext(0.5, 0.10, 'Settings', ha='center', fontsize=10, color='black')
 
-
-
-
-
-
-
-
-
-
-
-
-
-# Create frequency slider
+# Create Refractive Index slider
 ax_refractive_index = plt.axes([0.2, 0.02, 0.65, 0.01])
 refractive_index_slider = Slider(ax=ax_refractive_index, label='Material Refractive Index', valmin=1.0, valmax=4.0, valinit=initial_refractive_index, color='blue')
 refractive_index_slider.on_changed(update_refractive_index)
 
-# Create amplitude slider
+# Create Incident Angle slider
 ax_incoming_angle = plt.axes([0.2, 0.06, 0.65, 0.01])
-incoming_angle_slider = Slider(ax=ax_incoming_angle, label='Incoming Angle (Deg)', valmin=-90.0, valmax=89.9999, valinit=initial_incoming_angle, color='blue')
+incoming_angle_slider = Slider(ax=ax_incoming_angle, label='Incident Angle (Deg)', valmin=-90.0, valmax=89.9999, valinit=initial_incoming_angle, color='blue')
 incoming_angle_slider.on_changed(update_incoming_angle)
 
-# Create buttons
+# Create Laser on/off button
 ax_button_on_off = plt.axes([0.46, 0.17, 0.1, 0.05])
 button_on_off = Button(ax_button_on_off, 'TURN LASER\nON', color='lightblue', hovercolor='0.975')
 button_on_off.on_clicked(laser_on_off_switch)
 
+# Create two small outlined boxs on the figure to show the current values for angles and refractive index
+# Refractive Index Readouts
+left, width = -0.15, 0.3
+bottom, height = -0.27, 0.15
+right = left + width
+top = bottom + height
+data_box_1 = ax.text(right, top, 'Current Values\n\nAngle of Incidence: {:.2f}°\nRefractive Index: {:.2f}'.format(incoming_angle, refractive_index), horizontalalignment='right', verticalalignment='top', transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=1'))
+
+# Angle Readouts
+left, width = 0.53, 0.3
+right = left + width
+top = bottom + height
+data_box_2 = ax.text(right, top, 'Current Values\n\nAngle of Refraction: N/A°\nAngle of Reflection: N/A°', horizontalalignment='left', verticalalignment='top', transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=1'))
 
 
 
-### DRAW THE SIMICIRCLE OF TH EBOTTOM OF THE MATERIAL 
+
+### DRAW THE SIMICIRCLE OF THE BOTTOM OF THE MATERIAL 
 # Create angular values for half a circle (0 to 180 degrees)
 theta = np.linspace(-np.pi/2, np.pi/2, 100)
 
@@ -199,50 +191,21 @@ end_theta = np.deg2rad(-90)  # Ending angle in radians
 end_r = material_radius  # Ending radial value
 
 # Plot the straight line
-ax.plot([start_theta, end_theta], [start_r, end_r], 'k', alpha=0.5)  # Plot the half-circle
+ax.plot([start_theta, end_theta], [start_r, end_r], 'k', alpha=0.5)  # Plot the line
 
 
 
 
 
-# Initialize lines
-# Define the radial distance (r) and angle (theta) for the line
-r_line = 0.7  # Radial distance
-theta_line = np.deg2rad(0)  # Angle in radians
+# Initialise Rays
+incident_beam, = ax.plot([], [], 'lime', label='Laser', lw=2) 
+incident_glow, = ax.plot([], [], 'limegreen', lw=6, alpha=0.3, label='Glow')
 
-# Plot the line from the origin
-incident_beam, = ax.plot([0, theta_line], [0, r_line], 'lime', label='Laser', lw=2) 
-incident_glow, = ax.plot([0, theta_line], [0, r_line], 'limegreen', lw=6, alpha=0.3, label='Glow')
+reflected_beam, = ax.plot([], [], 'lime', label='Reflected Beam', lw=2)  
+reflected_glow, = ax.plot([], [], 'limegreen', lw=6, alpha=0.3, label='Glow')
 
-reflected_beam, = ax.plot([0, -theta_line], [0, r_line], 'lime', label='Reflected Beam', lw=2)  
-reflected_glow, = ax.plot([0, -theta_line], [0, r_line], 'limegreen', lw=6, alpha=0.3, label='Glow')
-
-refracted_beam, = ax.plot([0, 180-theta_line], [0, r_line], 'lime', label='Refracted Beam', lw=2)  
-refracted_glow, = ax.plot([0, 180-theta_line], [0, r_line], 'limegreen', lw=6, alpha=0.3, label='Glow')
-
-
-
-
-# Create a small outlined box on the figure to show the current values for angles and refractive index
-
-# Create a rectangle in axes coords
-left, width = -0.15, 0.3
-bottom, height = -0.27, 0.15
-right = left + width
-top = bottom + height
-data_box_1 = ax.text(right, top, 'Current Values\n\nAngle of Incidence: {:.2f}°\nRefractive Index: {:.2f}'.format(incoming_angle, refractive_index), horizontalalignment='right', verticalalignment='top', transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=1'))
-
-
-
-
-# Create a rectangle in axes coords
-left, width = 0.53, 0.3
-right = left + width
-top = bottom + height
-data_box_2 = ax.text(right, top, 'Current Values\n\nAngle of Refraction: N/A°\nAngle of Reflection: N/A°', horizontalalignment='left', verticalalignment='top', transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=1'))
-
-
-
+refracted_beam, = ax.plot([], [], 'lime', label='Refracted Beam', lw=2)  
+refracted_glow, = ax.plot([], [], 'limegreen', lw=6, alpha=0.3, label='Glow')
 
 
 
@@ -250,16 +213,12 @@ data_box_2 = ax.text(right, top, 'Current Values\n\nAngle of Refraction: N/A°\n
 
 
 # Setup plot
-ax.set_rlim(0, 1)  # Set the radial limits
+ax.set_title('Total Internal Reflection (TIR) Visualizer', y=1.05)
 ax.set_yticklabels([])  # This removes the labels on the radial axis without removing the ticks themselves
-title_y_position = 1.05  # Adjust this value to move the title up or down
-ax.set_title('Total Internal Reflection (TIR) Visualizer', y=title_y_position)
-#ax.legend(loc='upper right')
+ax.set_rlim(0, 1)  # Set the radial limits
 ax.grid(alpha=0.3)
 
-# Add the title above the bottom two sliders
-plt.figtext(0.5, 0.10, 'Settings', ha='center', fontsize=10, color='black')
 
 # Run Animation and show or save it
-anim = FuncAnimation(fig, animate, init_func=init, frames=None, interval=1/fps*1000, blit=True)   #interval *1000 to convert seconds to ms
+anim = FuncAnimation(fig, animate, init_func=init, frames=None, interval=1/fps*1000, blit=True, cache_frame_data=False)   #interval *1000 to convert seconds to ms
 plt.show()
